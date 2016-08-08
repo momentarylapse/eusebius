f1dee35d5650470b3845d8d839b2d0845b9b43a3
Michael Ankele <michi@lupina.de>
2016-08-09 01:23:45 +0200

    many minor things...

--- bochs/run-qemu.sh
+++ bochs/run-qemu.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -boot c -m 512 -hda 'c.img' -net nic,vlan=0 -net user,vlan=0 -localtime -soundhw sb16 -smp 4 -vga std
+qemu-system-x86_64 -boot c -m 512 -drive file='c.img',index=0,media=disk,format=raw -net nic,vlan=0 -net user,vlan=0 -localtime -soundhw sb16 -smp cpus=2,cores=2 -vga std
