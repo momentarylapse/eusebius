81b4439517fbe78dafc746c70dee6646685da14f
Michael Ankele <michi@lupina.de>
2018-02-27 23:05:47 +0100

    xfiles with backspace

--- bochs/run-qemu.sh
+++ bochs/run-qemu.sh
@@ -7,6 +7,8 @@ qemu-system-x86_64 -boot c -m 512 -drive file='c.img',index=0,media=disk,format=
  -smp cpus=2,cores=2 \
  -vga std
 # -device virtio-vga,virgl=on
+# -soundhw hda \
+# -soundhw sb16 \
 
 # -net nic,model=ne2k_pci -netdev user,id=eusebius
 
