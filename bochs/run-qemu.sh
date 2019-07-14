cdb7bf5024ea574f611cc7c96d8197b528d228d7
Michael Ankele <michi@lupina.de>
2019-07-14 21:34:02 +0200

    qemu parameter changed

--- bochs/run-qemu.sh
+++ bochs/run-qemu.sh
@@ -2,7 +2,6 @@
 #qemu-system-x86_64 -boot c -m 512 -drive file='c.img',index=0,media=disk,format=raw -net nic,model=ne2k_pci -netdev user,id=eusebius -localtime -soundhw sb16 -smp cpus=2,cores=2 -vga std
 qemu-system-x86_64 -boot c -m 512 -drive file='c.img',index=0,media=disk,format=raw \
  -netdev user,id=network0 -device ne2k_pci,netdev=network0,mac=52:54:00:12:34:56 \
- -localtime \
  -soundhw sb16 \
  -smp cpus=2,cores=2 \
  -vga std
