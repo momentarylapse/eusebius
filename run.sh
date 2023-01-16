4839a18d969511ba0ff850585c808734a4922c05
Michael Ankele <michi@lupina.de>
2023-01-16 02:14:30 +0100

    some GDT experiments

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 #qemu-system-x86_64 -boot c -m 512 -drive file='bochs/c.img',index=0,media=disk,format=raw -net nic,model=ne2k_pci -netdev user,id=eusebius -localtime -soundhw sb16 -smp cpus=2,cores=2 -vga std
 
-qemu-system-x86_64 -boot c -m 512 -drive file='bochs/c.img',index=0,media=disk,format=raw \
- -netdev user,id=network0 -device rtl8139,netdev=network0,mac=52:54:00:12:34:56 \
- -device AC97 \
+qemu-system-x86_64 \
+ -boot c \
+ -m 512 \
+ -drive file='bochs/c.img',index=0,media=disk,format=raw \
+### not now... takes too much time to reset between runs
+### -netdev user,id=network0 -device rtl8139,netdev=network0,mac=52:54:00:12:34:56 \
+### -device AC97 \
  -smp cpus=2,cores=2 \
  -vga std
 # -device virtio-vga,virgl=on
