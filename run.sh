7c5402a74a83599ffb769da8eaab9240343917a9
Michael Ankele <michi@lupina.de>
2023-02-21 16:35:57 +0100

    kaba update

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ qemu-system-x86_64 \
  -boot c \
  -m 512 \
  -drive file='bochs/c.img',index=0,media=disk,format=raw \
+ -vga std
 ### not now... takes too much time to reset between runs
 ### -netdev user,id=network0 -device rtl8139,netdev=network0,mac=52:54:00:12:34:56 \
 ### -device AC97 \
 ### -smp cpus=2,cores=2 \
- -vga std
 # -device virtio-vga,virgl=on
 # -soundhw hda \
 # -soundhw sb16 \
